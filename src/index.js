5189ed19c84b09ff977410a8e2014e6e1fdb5ece
FIRST_NAME LAST_NAME <joshuadiboll@gmail.com>
2017-04-13 10:46:01 -0400

    simplified file structure. Created server.

--- src/index.js
+++ src/index.js
@@ -6,15 +6,13 @@ var app = express();
 var jsonData = {count: 12, message: "Hey"}
 
 
- function onRequest(request, response) {
 	app.get('/', function (req, res) {
 	res.sendFile(__dirname + '/index.html', function(err) {
 		if(err) {
 			res.status(500).send(err);
 		}
 	})
-})
-};
+});
 
 app.post('/data', function(req, res) {
 	res.json(jsonData);
